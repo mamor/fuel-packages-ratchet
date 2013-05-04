819cc3d0911584c11e16c06ed183823111b84d5d
mamor <mamor.dev@gmail.com>
2013-05-04 18:00:38 +0900

    fixed loading autoload.php

--- tasks/ratchet.php
+++ tasks/ratchet.php
@@ -2,7 +2,14 @@
 
 namespace Fuel\Tasks;
 
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php'))
+{
+	require_once __DIR__.'/../vendor/autoload.php';
+}
+else
+{
+	require_once VENDORPATH.'autoload.php';
+}
 
 /**
  * Task of Ratchet Server
